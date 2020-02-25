f3a1e45613fb874e10f173d725fee1a3a490e354
Alexiss Diaz <alexiss.diaz@live.com>
2020-02-25 00:06:35 -0600

    added react router in app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </div>
+    </Router>
   );
 }
 
