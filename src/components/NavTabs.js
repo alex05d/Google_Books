eaa19cb585faee4ac874d7c642c1d60d8424ace0
Alexiss Diaz <alexiss.diaz@live.com>
2020-02-25 00:29:02 -0600

    added navtabs component

---
+++ src/components/NavTabs.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavTabs() {
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/search" className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}>
+                    Search
+                    </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/saved" className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
+                    Saved
+                </Link>
+            </li>
+        </ul>
+    );
+}
+
+export default NavTabs;
